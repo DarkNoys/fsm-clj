7367fa3d3268ab779486a331088363432393207e
DarkNoys <s-paradox@ro.ru>
2018-10-26 20:49:18 +0300

    move to leiningen

---
+++ doc/intro.md
@@ -0,0 +1,2 @@
+# Introduction to fsm-clj
+
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject fsm-clj "0.1.0-SNAPSHOT"
+  :description "Finite-state machine framework on Clojure"
+  :url "https://github.com/DarkNoys/fsm-clj"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [clj-time "0.14.4"]
+                 [org.clojure/core.async "0.4.474"]])
